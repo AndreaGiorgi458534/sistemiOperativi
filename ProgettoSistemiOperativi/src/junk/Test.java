96ffcfdf3d81fbd648c2f624c652bb411b164853
Andrea <30377977+TreCani@users.noreply.github.com>
2020-07-23 13:48:57 +0200

    Creazione del progetto

---
+++ ProgettoSistemiOperativi/src/junk/Test.java
@@ -0,0 +1,12 @@
+package junk;
+
+import javax.swing.JFrame;
+
+public class Test {
+	
+	public static void main(String[] args)  {
+		System.out.println("Helo");
+		JFrame g = new JFrame();
+		g.setVisible(true);
+	}
+}
